6f9bf6a7b453893feae6d3600a815247443265fe
ThomasDuverney <thomas.duverney.officiel@gmail.com>
2016-05-26 16:23:32 +0200

    modif putain contacts

---
+++ ajax/AutresContact.class.php
@@ -0,0 +1,10 @@
+ <?php
+	require_once('Personne.class.php');
+    class AutresContacts extends Personne {
+        public $metier;   // string. metier du contact
+        public $type;  // classe énumerer 'type'. type de contact
+	public $groupes; //tableau. les différents groupe dont le contact fait partie.
+    }
+
+
+?>
