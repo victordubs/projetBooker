533d1b27052479d199a24bd0778c4244a28a397e
LYONS Liam <lyonsl@hostname.iut2.upmf-grenoble.fr>
2016-06-01 15:42:57 +0200

    MAJ INSERTION

--- ajax/saveArtiste.php
+++ ajax/saveArtiste.php
@@ -4,14 +4,13 @@
 	$result["status"] = "success" ;
 
 	// Normalement, ici : insertion ou mise � jour de la base
-	//$role = array("batteur","chanteur");
-	$groupes = NULL;
+	$roles = json_decode($_REQUEST["roles"],true);
 
 	if (!isset($_REQUEST['idp'])) {
 		var_dump("Dans le INSERT");
 		$result = $dao->getMaxIdPlus1Artiste();
 		$_REQUEST['idp']=$result;
-		$dao->insertArtiste($_REQUEST['mail'],$_REQUEST['tel'],$_REQUEST['siteWeb'],$_REQUEST['idp'],$_REQUEST['ville'],$_REQUEST['adresse'],$_REQUEST['nom'],$_REQUEST['prenom'],$_REQUEST['roles'],$groupes);
+		$dao->insertArtiste($_REQUEST['mail'],$_REQUEST['tel'],$_REQUEST['siteWeb'],$_REQUEST['idp'],$_REQUEST['ville'],$_REQUEST['adresse'],$_REQUEST['nom'],$_REQUEST['prenom'],$roles,$groupes);
 	}
 
 	else {
